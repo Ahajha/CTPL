3da1c21cc47cd2ed4d5690974498f880bfb4c16f
Alex Trotta <ahajha@gmail.com>
2021-02-02 16:12:04 -0500

    Add modification copyright

--- ctpl_stl.h
+++ ctpl_stl.h
@@ -1,6 +1,7 @@
 /*********************************************************
 *
 *  Copyright (C) 2014 by Vitaliy Vitsentiy
+*  Modifications: Copyright (C) 2021 by Alex Trotta
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
