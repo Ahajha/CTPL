36246038fca5b393c10431c5ed2fc1576edd889c
Alex <ahajha@gmail.com>
2021-02-04 03:06:51 -0500

    rewrite example program

--- example.cpp
+++ example.cpp
@@ -1,133 +1,190 @@
 #include "ctpl_stl.h"
 #include <iostream>
 #include <string>
+#include <mutex>
 
-void first(int id) {
-    std::cout << "hello from " << id << ", function\n";
-}
+std::mutex iomut;
 
-void aga(int id, int par) {
-    std::cout << "hello from " << id << ", function with parameter " << par <<'\n';
-}
-
-struct Third {
-    Third(int v) { this->v = v; std::cout << "Third ctor " << this->v << '\n'; }
-    Third(Third && c) { this->v = c.v; std::cout<<"Third move ctor\n"; }
-    Third(const Third & c) { this->v = c.v; std::cout<<"Third copy ctor\n"; }
-    ~Third() { std::cout << "Third dtor\n"; }
-    int v;
-};
-
-void mmm(int id, const std::string & s) {
-    std::cout << "mmm function " << id << ' ' << s << '\n';
-}
-
-void ugu(int id, Third & t) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    std::cout << "hello from " << id << ", function with parameter Third " << t.v <<'\n';
-}
-
-struct A
+void f1(int id)
 {
-	A() { std::cout << "Default constructor\n"; }
-	~A() { std::cout << "Destructor\n"; }
-	A(A&&) { std::cout << "Move constructor\n"; }
-	A(const A&) { std::cout << "Copy constructor\n"; }
-	A& operator=(A&&) { std::cout << "Move assignment\n"; return *this; }
-	A& operator=(const A&) { std::cout << "Copy assignment\n"; return *this; }
+	std::lock_guard<std::mutex> lock(iomut);
+	std::cout << "f1, thread #" << id << '\n';
+}
+
+void f2(int id, int par)
+{
+	std::lock_guard<std::mutex> lock(iomut);
+	std::cout << "f2, thread #" << id << ", parameter " << par <<'\n';
+}
+
+void f3(int id, const std::string & s)
+{
+	std::lock_guard<std::mutex> lock(iomut);
+	std::cout << "f3, thread #" << id << ", '" << s << "'\n";
+}
+
+template<class T>
+struct S1
+{
+	T val;
+	
+	S1(T v) : val(v)
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 ctor: " << this->val << '\n';
+	}
+	
+	S1(S1 && c) : val(c.val)
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 move ctor\n";
+	}
+	
+	S1(const S1& c) : val(c.val)
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 copy ctor\n";
+	}
+	
+	S1& operator=(S1&& c)
+	{
+		val = c.val;
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 move assign\n"; return *this;
+	}
+	
+	S1& operator=(const S1& c)
+	{
+		val = c.val;
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 copy assign\n";
+		return *this;
+	}
+	~S1()
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 dtor\n";
+	}
+	
+	void operator()(int id) const
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "S1 functor, thread #" << id << ", val = " << val << '\n';
+	}
 };
 
-void fA(int, const A&) { std::cout << "got an A\n"; }
+void f4(int id, S1<int> & s)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	std::lock_guard<std::mutex> lock(iomut);
+	std::cout << "f4, thread #" << id << ", parameter S1 = " << s.val << '\n';
+}
 
-int main(int argc, char **argv) {
-    ctpl::thread_pool p(2 /* two threads in the pool */);
+void f5(int id, S1<int> s)
+{
+	std::lock_guard<std::mutex> lock(iomut);
+	std::cout << "f5, thread #" << id << ", parameter S1 = " << s.val << '\n';
+}
 
-    std::future<void> qw = p.push(std::ref(first));  // function
-    p.push(first);  // function
-    p.push(aga, 7);  // function
-
-    {
-        struct Second {
-            Second(const std::string & s) { std::cout << "Second ctor\n"; this->s = s; }
-            Second(Second && c) { std::cout << "Second move ctor\n"; s = std::move(c.s); }
-            Second(const Second & c) { std::cout << "Second copy ctor\n"; this->s = c.s; };
-            ~Second() { std::cout << "Second dtor\n"; }
-            void operator()(int id) const {
-                std::cout << "hello from " << id << ' ' << this->s << '\n';
-            }
-        private:
-            std::string s;
-        } second(", functor");
-
-        p.push(std::ref(second));  // functor, reference
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-        p.push(const_cast<const Second &>(second));  // functor, copy ctor
-        p.push(std::move(second));  // functor, move ctor
-        p.push(second);  // functor, move ctor
-        p.push(Second(", functor"));  // functor, move ctor
-    }
-        {
-            Third t(100);
-
-            p.push(ugu, std::ref(t));  // function. reference
-            p.push(ugu, t);  // function. copy ctor, move ctor
-            //p.push(ugu, std::move(t));  // function. move ctor, move ctor
-
-        }
-        
-        //p.push(ugu, Third(200));  // function
-
-
-
-    std::string s = ", lambda";
-    p.push([s](int id){  // lambda
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-        std::cout << "hello from " << id << ' ' << s << '\n';
-    });
-
-    p.push([s](int id){  // lambda
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-        std::cout << "hello from " << id << ' ' << s << '\n';
-    });
-
-    p.push(mmm, "worked");
-
-    auto f = p.pop();
-    if (f) {
-        std::cout << "poped function from the pool ";
-        f(0);
-    }
-    // change the number of treads in the pool
-
-    p.resize(1);
-
-    std::string s2 = "result";
-    auto f1 = p.push([s2](int){
-        return s2;
-    });
-    // other code here
-    //...
-    std::cout << "returned " << f1.get() << '\n';
-
-    auto f2 = p.push([](int){
-        throw std::exception();
-    });
-    // other code here
-    //...
-    try {
-        f2.get();
-    }
-    catch (std::exception & e) {
-        std::cout << "caught exception\n";
-    }
-
-    // get thread 0
-    auto & th = p.get_thread(0);
-    
-    // Test perfect forwarding.
-    p.push(fA, A{});
-    A a;
-    p.push(fA, a);
-    
-    return 0;
+int main(int argc, char **argv)
+{
+	// Create pool with 4 threads
+	ctpl::thread_pool p(4);
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Pushing functions\n";
+	}
+	{
+		std::future<void> fut = p.push(std::ref(f1));
+		p.push(f1);
+		p.push(f2, 7);
+	}
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Pushing functors\n";
+	}
+	{
+		S1<int> func(100);
+		
+		p.push(std::ref(func));  // functor, reference
+		
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		
+		p.push(const_cast<const S1<int> &>(func));  // functor, copy ctor
+		p.push(std::move(func));  // functor, move ctor
+		p.push(func);  // functor, move ctor
+		p.push(S1<std::string>("string version"));  // functor, move ctor
+	}
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Pushing lambdas\n";
+	}
+	{
+		std::string s = "lambda";
+		
+		for (unsigned i = 0; i < 16; ++i)
+		{
+			{
+				std::lock_guard<std::mutex> lock(iomut);
+				std::cout << "pushing lambda #" << i << '\n';
+			}
+			p.push([s](int id)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+				std::lock_guard<std::mutex> lock(iomut);
+				std::cout << "lambda, thread #" << id << ' ' << s << '\n';
+			});
+		}
+	}
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Resizing the pool\n";
+	}
+	p.resize(8);
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Testing future returns\n";
+	}
+	{
+		auto f1 = p.push([](int) { return 5; });
+		
+		auto result = f1.get();
+		
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "returned " << result << '\n';
+	}
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Testing future exception catching\n";
+	}
+	{
+		auto f2 = p.push([](int) { throw std::exception(); });
+			
+		try
+		{
+			f2.get();
+		}
+		catch (std::exception & e)
+		{
+			std::lock_guard<std::mutex> lock(iomut);
+			std::cout << "caught exception\n";
+		}
+	}
+	
+	{
+		std::lock_guard<std::mutex> lock(iomut);
+		std::cout << "Testing perfect forwarding\n";
+	}
+	{
+		S1<int> s{0};
+		p.push(f4, s);
+		
+		p.push(f5, S1<int>{0});
+	}
 }
